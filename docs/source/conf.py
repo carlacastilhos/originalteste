e050368b5195bb7f2e76f0ad533b77923f0434c1
Carla Castilhos <carla.castilhos@anm.gov.br>
2023-10-18 14:12:41 -0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'FAQ Protocolo Digital ANM'
+copyright = '2021, ANM'
+author = 'ANM'
 
 release = '0.1'
 version = '0.1.0'
